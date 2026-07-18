cb8838e6cca301c7f19703e981b37ee10931c680
Kunal More <kunal@ex.com>
2026-07-18 12:00:03 +0530

    Changes for Not Found error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['walrus-luxury-gnarly.ngrok-free.dev'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false,
       },
     },
   },
